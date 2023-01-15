adb690ab6fa8d66c01d447662546660da9a9c202
dracero <diego.racero@gmail.com>
2023-01-15 17:05:07 -0300

    domingo

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts,glb,gltf,bin}']
 	}
 };
 
