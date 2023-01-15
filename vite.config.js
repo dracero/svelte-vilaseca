9a8933531b962128bbcfefec90672619bbac807c
dracero <diego.racero@gmail.com>
2023-01-15 17:55:44 -0300

    domingo

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 const config = {
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts,glb,gltf,bin}']
+		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 };
 
-export default config;
+
+
+export default config
+
+//ejecutar:
+/* npm i -D @sveltejs/adapter-vercel@next
+
+y luego cambiar el config por el archivo de más arriba*/
